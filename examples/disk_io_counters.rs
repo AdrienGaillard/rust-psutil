c9084bfdf9b3400ef2301a9bb909da5f2d02d121
Adrien Gaillard <adrien.gaillard@bleemeo.com>
2018-07-18 12:37:05 +0200

    Example changes

--- examples/disk_io_counters.rs
+++ examples/disk_io_counters.rs
@@ -6,10 +6,18 @@ fn main() {
     let mut disk_io_counters_collector = psutil::disk::DiskIOCountersNoWrap::new();
 
     loop {
-        let block_time = time::Duration::from_millis(2000);
+        let past_disk_io_counters = match disk_io_counters_collector.disk_io_counters(true) {
+            Ok(disk_io_counters) => disk_io_counters,
+            Err(_) => {
+                println!("Could not loading disk informations");
+                continue;
+            }
+        };
+
+        let block_time = time::Duration::from_millis(1000);
         thread::sleep(block_time);
 
-        let disk_io_counters = match disk_io_counters_collector.disk_io_counters(true) {
+        let current_disk_io_counters = match disk_io_counters_collector.disk_io_counters(true) {
             Ok(disk_io_counters) => disk_io_counters,
             Err(_) => {
                 println!("Could not loading disk informations");
@@ -19,24 +27,11 @@ fn main() {
 
         println!(
             "Disk general usage:
-            read_count:         {}
-            write_count:        {}
-            read_bytes:         {}
-            write_bytes:        {}
-            read_time:          {}
-            write_time:         {}
-            read_merged_time:   {}
-            write_merged_time:  {}
-            busy_time:          {}",
-            disk_io_counters.read_count,
-            disk_io_counters.write_count,
-            disk_io_counters.read_bytes,
-            disk_io_counters.write_bytes,
-            disk_io_counters.read_time,
-            disk_io_counters.write_time,
-            disk_io_counters.read_merged_count,
-            disk_io_counters.write_merged_count,
-            disk_io_counters.busy_time,
+            read_bytes:         {} Bytes/s
+            write_bytes:        {} Bytes/s
+            ",
+            (current_disk_io_counters.read_bytes - past_disk_io_counters.read_bytes),
+            (current_disk_io_counters.write_bytes - past_disk_io_counters.write_bytes),
         );
     }
 }
